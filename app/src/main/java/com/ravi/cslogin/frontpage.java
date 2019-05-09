535fac883afcb5eaa106abf819da6163ea72a371
007ravi <mishra007ravi@gmail.com>
2019-05-09 20:47:50 +0530

    front

---
+++ app/src/main/java/com/ravi/cslogin/frontpage.java
@@ -0,0 +1,13 @@
+package com.ravi.cslogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class frontpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frontpage);
+    }
+}
